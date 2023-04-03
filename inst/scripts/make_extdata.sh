e70d18d8761015f2fbd647d5998c154b27fe1da2
js2264 <jacquesserizay@gmail.com>
2023-04-03 13:58:20 +0200

    finish topological features chapter

--- inst/scripts/make_extdata.sh
+++ inst/scripts/make_extdata.sh
@@ -1,4 +1,4 @@
 cooler dump -t chroms /home/rsg/.cache/R/fourDNData/4d434d8538a0_4DNFI9FVHJZQ.mcool::/resolutions/250000 | grep -P 'chr17\t' > inst/extdata/chromsizes
 cooler dump -t pixels -r chr17 --join /home/rsg/.cache/R/fourDNData/4d434d8538a0_4DNFI9FVHJZQ.mcool::/resolutions/5000 > inst/extdata/chr17_5000.bg2
 cooler cload pairs inst/extdata/chromsizes:5000 inst/extdata/chr17_5000.bg2 -c1 1 -p1 2 -c2 4 -p2 5 -0 inst/extdata/chr17.cool
-cooler zoomify --resolutions 5000,250000 --balance inst/extdata/chr17.cool -o inst/extdata/chr17.mcool
+cooler zoomify --resolutions 5000,100000,250000 --balance inst/extdata/chr17.cool -o inst/extdata/chr17.mcool
